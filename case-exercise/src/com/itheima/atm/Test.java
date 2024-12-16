50bc554862cbf7852b2f2f6e58b912de629828bb
shawn <shawn.zzz@foxmail.com>
2024-12-16 17:02:34 +0800

    git知识&ATM项目

---
+++ case-exercise/src/com/itheima/atm/Test.java
@@ -0,0 +1,11 @@
+package com.itheima.atm;
+
+public class Test {
+    public static void main(String[] args) {
+        // 测试类
+        // 1.启动
+        ATM atm = new ATM();
+        atm.start();
+
+    }
+}
